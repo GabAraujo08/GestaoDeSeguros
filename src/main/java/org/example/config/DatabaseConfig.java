a4f4b0de6b49b04c9d1b646813dcec63302863d5
Gabrielly Macedo <162237578+gabimaced0@users.noreply.github.com>
2024-10-15 22:50:59 -0300

    padrao singleton na classe DatabaseConfig

--- src/main/java/org/example/config/DatabaseConfig.java
+++ src/main/java/org/example/config/DatabaseConfig.java
@@ -5,20 +5,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
+    private static DatabaseConfig instance;
     private final String url;
     private final String user;
     private final String password;
     private Connection connection;
 
-    public DatabaseConfig(String url, String user, String password) {
+    private DatabaseConfig(String url, String user, String password) {
         this.url = url;
         this.user = user;
         this.password = password;
     }
-
+    public static synchronized DatabaseConfig getInstance(String url, String user, String password) {
+        if (instance == null) {
+            instance = new DatabaseConfig(url, user, password);
+        }
+        return instance;
+    }
+    
     public Connection getConnection() throws SQLException {
-        connection = DriverManager.getConnection(url, user, password);
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(url, user, password);
+        }
         return connection;
     }
 
+    public void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
 }
